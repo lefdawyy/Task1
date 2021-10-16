f41712b7c74e5a4dd7354b8ab2d7e60d022ac5d1
lefdawyy <92638982+lefdawyy@users.noreply.github.com>
2021-10-16 18:48:59 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,48 @@
+function nablus(){
+    document.getElementById('info-location').innerHTML="قاعة حمدي منكو"
+    document.getElementById('info-number').innerHTML="092380399"
+}
+function jericho(){
+    document.getElementById('info-location').innerHTML="الجفتلك"
+    document.getElementById('info-number').innerHTML="022321170"
+}
+function hrbron(){
+    document.getElementById('info-location').innerHTML="مديرية صحة الخليل"
+    document.getElementById('info-number').innerHTML="022227288"
+}
+function jerusalem(){
+    document.getElementById('info-location').innerHTML="الرام"
+    document.getElementById('info-number').innerHTML="025848010- 9110*"
+}
+function bitlahm(){
+    document.getElementById('info-location').innerHTML="مديرية صحة بيت لحم"
+    document.getElementById('info-number').innerHTML="02744222"
+}
+function jenin(){
+    document.getElementById('info-location').innerHTML="اليامون"
+    document.getElementById('info-number').innerHTML="042502601"
+}
+function ramallah(){
+    document.getElementById('info-location').innerHTML="مجمع رام الله الترويحي"
+    document.getElementById('info-number').innerHTML="022400666"
+}
+function silfeet(){
+    document.getElementById('info-location').innerHTML="عيادة بديا"
+    document.getElementById('info-number').innerHTML="092515101"
+}
+function tubas(){
+    document.getElementById('info-location').innerHTML="مديرية صحة طوباس"
+    document.getElementById('info-number').innerHTML="092574882"
+}
+function tulkarm(){
+    document.getElementById('info-location').innerHTML="مديرية صحة طولكرم"
+    document.getElementById('info-number').innerHTML="092672140"
+}
+function qalqeliah(){
+    document.getElementById('info-location').innerHTML="مركز القلب الكبير"
+    document.getElementById('info-number').innerHTML="092940440"
+}
+function gaza(){
+    document.getElementById('info-location').innerHTML="مديرية صحة قطاع غزة"
+    document.getElementById('info-number').innerHTML="082833333"
+}
